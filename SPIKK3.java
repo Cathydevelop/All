8ee5b005048eea3aeeeba9c5cbcd6941d90ac95c
refocus <refocustest@gmail.com>
2017-01-22 23:12:48 +0200

    Update SPIKK3.java

--- SPIKK3.java
+++ SPIKK3.java
@@ -9,7 +9,23 @@ WWW
 CODES https://github.com/caveofprogramming/java-beginners.git
 
 
-OOP 
-konstruktor klass konstruktor meetod
+
+OOP konstruktor klass konstruktor meetod
+OOP Pood-Kassa
+//1. Pood seob end teise klassiga järgnevalt, muu pole tähtis. Omistab kassapidaja sulgudes
+Kassa kassa = new Kassa(kassapidaja);
+//2.1 Kassa nimetab kohe ära objekt-kassapidaja et see oleks meetodist VÄLJAS
+public String kassapidaja;                  //tegelasobjekt
+//2.2 Põhimeetod konstuktor, mille meetodi sisse pannakse sidumiseks: kassapidaja
+public Kassa(String kassapidajaSaabSiinUueNime){kassapidaja = kassapidajaSaabSiinUueNime;}
+
+OOP MAATRIKS
+1. Generate Matrix - start
+2. Output this Matrix - end
+
+
+  
+  
+
 
 throws Exception - vea kontroll funk
