4859959148a5941c5e7508b8b063af416ae7ed17
refocus <refocustest@gmail.com>
2017-01-26 23:46:41 +0200

    Update SPIKK3.java

--- SPIKK3.java
+++ SPIKK3.java
@@ -1,6 +1,39 @@
+ASK HELP SITES:
+http://stackoverflow.com
+
+
+CODES 
 https://github.com/caveofprogramming/java-beginners.git 
+Java operators
 https://www.tutorialspoint.com/java/java_basic_operators.htm
-from my Safari:
+https://en.m.wikipedia.org/wiki/List_of_Java_keywords
+TheNewBoston BUCKY
+https://www.youtube.com/watch?v=5u8rFbpdvds&index=2&list=PL17E300C92CE0261A
+Krister Viirsaar
+http://i200.itcollege.ee/#
+https://www.youtube.com/watch?v=_mFnk9S1pXg&list=PLWkgPad9kpY3IWcuywjrCVQgFiF5MhEMp&index=19
+https://www.youtube.com/playlist?list=PLWkgPad9kpY1Ns3Q23g1qAlE9vQbmpSKn
+http://kommuun.koodikool.ee/t/projekt-puu-vanuse-arvutamine/204
+KEYBOARD OPs
+https://wiki.itcollege.ee/index.php/Keyboard_shortcuts
+usage IntelliJ
+https://www.jetbrains.com/help/idea/2016.2/keyboard-shortcuts-you-cannot-miss.html
+OOP teooria eesti keeles
+https://et.wikipedia.org/wiki/Objektorienteeritud_programmeerimine
+http://enos.itcollege.ee/~jpoial/java/i200loeng3.html
+INFO JK
+http://minitorn.tlu.ee/~jaagup/kool/java/naited20/io/fail/juht.html
+õppimist, boolean:
+https://www.dotnetperls.com/boolean-java
+õppimist, Arrays:
+http://docs.oracle.com/javase/6/docs/api/java/util/Arrays.html
+õppimist, event handler:
+http://docs.oracle.com/javase/8/javafx/events-tutorial/convenience_methods.htm#BABCCIHI
+õppimist, javaFX:
+http://docs.oracle.com/javafx/2/get_started/jfxpub-get_started.htm
+õppimist,
+http://howtodoinjava.com/for-fun-only/best-way-to-learn-java/
+http://java.about.com
 
 
 OOP konstruktor klass konstruktor meetod
@@ -20,16 +53,5 @@ OOP MAATRIKS
  
  EXTENDS class Neo saab fighther omadused, overwright
  
-i200 1-1 Sissejuhatus: i200, kuidas täiendada; õppetöö online vorm, eesmärk 3 kuuga nooremprogrameerijaks; oma projekt ja iganädalased ülessanded, õppematerjal ja kommuunikool koodiõppimiseks foorumis
-i200 1-2 Programmeerimisest üldisemalt
-i200 1-3 Git ja Laevade Pommitamise algus: Git vajalikud bash käsud koodi pushimiseks näiteks GitHub kontosse
-i200 2-1 Loogilised operaatorid ja booleanid: BOOLEAN true false IF jaoks, !a NB!HÜÜMÄRK SEE ON BOOLEANITE OMADUS, && (and) || (or)
-https://www.youtube.com/watch?v=gjxBTo37n8M&index=4&list=PLWkgPad9kpY3IWcuywjrCVQgFiF5MhEMp
-//https://www.tutorialspoint.com/java/java_basic_operators.htm
-    //ternary operator operation in java
-5-1 Objektorienteeritus: klasside loomine, for-tsükkel, mida meetod võtab vastu ja mida tagastab ehk programmi loogika; 
-näited, kuidas kutsud (punktiga) välja teise klassi meetodeid ja väärtusi;
-public vs private, protected
 
-https://www.youtube.com/watch?v=cGUMn0gzWvg&index=14&list=PLWkgPad9kpY3IWcuywjrCVQgFiF5MhEMp
  
