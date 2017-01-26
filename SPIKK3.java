927076c80d9c110a2ff6f79a9069e5e89bb3feb9
refocus <refocustest@gmail.com>
2017-01-26 07:32:21 +0200

    Update SPIKK3.java

--- SPIKK3.java
+++ SPIKK3.java
@@ -1,7 +1,6 @@
-WWW-avatult:
-***Eksamiks
-***Lihtne kood, minu kood
-***GitHub-leitud1/java-beginners/  KEERULINE https://github.com/caveofprogramming/java-beginners.git 
+https://github.com/caveofprogramming/java-beginners.git 
+https://www.tutorialspoint.com/java/java_basic_operators.htm
+from my Safari:
 
 
 OOP konstruktor klass konstruktor meetod
@@ -26,5 +25,11 @@ i200 1-2 Programmeerimisest üldisemalt
 i200 1-3 Git ja Laevade Pommitamise algus: Git vajalikud bash käsud koodi pushimiseks näiteks GitHub kontosse
 i200 2-1 Loogilised operaatorid ja booleanid: BOOLEAN true false IF jaoks, !a NB!HÜÜMÄRK SEE ON BOOLEANITE OMADUS, && (and) || (or)
 https://www.youtube.com/watch?v=gjxBTo37n8M&index=4&list=PLWkgPad9kpY3IWcuywjrCVQgFiF5MhEMp
+//https://www.tutorialspoint.com/java/java_basic_operators.htm
+    //ternary operator operation in java
+5-1 Objektorienteeritus: klasside loomine, for-tsükkel, mida meetod võtab vastu ja mida tagastab ehk programmi loogika; 
+näited, kuidas kutsud (punktiga) välja teise klassi meetodeid ja väärtusi;
+public vs private, protected
 
+https://www.youtube.com/watch?v=cGUMn0gzWvg&index=14&list=PLWkgPad9kpY3IWcuywjrCVQgFiF5MhEMp
  
