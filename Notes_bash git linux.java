58dd381ac1692fe8fb2f759d395f953f966c33ff
refocus <refocustest@gmail.com>
2017-01-27 22:26:02 +0200

    Update Notes_bash git linux.java

--- Notes_bash git linux.java
+++ Notes_bash git linux.java
@@ -258,6 +258,243 @@ http://stackoverflow.com/questions/5617211/what-is-git-remote-add-and-git-push-o
 
 
 
+AINE = IT EELKURSUS... SOFTs (main) + ⌘ MAC
+"Arvestus"
+HINDED: http://enos.itcollege.ee/~lvosandi/check.html > I253 grading bot
+
+MAIN: https://wiki.itcollege.ee/index.php/I253_Informatika_eelkursus
+
+BY: 
+Lauri Võsandi, Arti Zirk, Marvin Martinson, Kristjan Peterson, Taavi Teearu, Mohanad Aly 
+by Arti Zirk https://git.wut.ee/arti/i237   arti.zirk@gmail   git.wut.ee/arti/koduleht/
+https://wiki.itcollege.ee/index.php/I253_Informatika_eelkursus
+https://wiki.itcollege.ee/index.php/I253_Presessional_Informatics (DETAILSEM)
+https://echo360.e-ope.ee/ess/portal/section/fbde1292-f109-46fe-88de-33d0257c0e19 https://echo360.e-ope.ee/ess/portal/section/6435daaf-a9c9-4edf-b257-6dfe17590a0d
+
+MINU KOOLIKODUKAS: http://enos.itcollege.ee/~krist/  (SFTP 22)
+enos.itcollege.ee/~krist/PROOV/
+MINU KOOLIARVUTIS:
+/home/krist/public_html - siin ongi minu kodukas vist ikkagi Documents all
+http://enos.itcollege.ee/~krist/index.html
+/shared - siin on programme
+
+Sisselogimine kooli Linux Ubuntusse: 
+student/ studnet, kui ei taha enda parooli kasutada
+
+HEA ABIKS https://www.mediawiki.org/wiki/Help:Formatting
+WIKI: https://wiki.itcollege.ee/index.php/User:Krist
+………………………………
+HOW TO: without mouse:
+Selekteerida
+Aknaraami lohistamine
+
+
+
+………………………………..
+SOFTs COLLECTIONS:
+;;;OS, Windows, Linux Ubuntu
+;;;Terminal, PuTTY, Zoc6
+;;;bitbucket = github git.wut.ee  , git.wut.ee/arti/koduleht/
+;;;Eclipse vs Intelli J, Sublime text, XCode = Notepad++
+;;;FileZilla vs Cyberduck
+;;;www: W3schools
+;;;Cinema 4D, 3DMAX,
+;;;Sequel pro for sql = Heidi sql?
+MATA : apps MALMATH, PHOTOMATH, WOLFRAM ALFA
+NetBeans
+;;;GIT versioonihaldustarkvara
+;;;Win10, One Drive, Office 365 - mulle tasuta, terra ruumi; Word, Excel of 2016, OneNote, Outlook, PPT
+…………………………………………………………………..
+WIndows10 ja Ubuntu allalaadimiseks SOFT siit: http://upload.itcollege.ee/iso/   saad kolmeks kuuks - ÄRA VÕTA WIN sealt, mujalt saad eluks ajaks
+siit saad windows10 aga tasuta: https://e5.onthehub.com/WebStore/ProductsByMajorVersionList.aspx?cmi_cs=1&cmi_mnuMain=bdba23cf-e05e-e011-971f-0030487d8897&ws=6ece1175-619b-e011-969d-0030487d8897&vsro=8   võta see versioon : Windows 10 Multiple Editions 32/64-bit (English) - Microsoft Imagine. Pane oma kooli EMAILI AADRESS Microsoft Web ostupoes. Saadeti kontrollemail. Lõpeta konto loomine, mis oli suht tülikas protsess.
+order number:   100502814429; Product Key: K2944-NXXWQ-9CBH2-J88V4-9W3GT; Microsoft Imagine EULA    Agreement Number: 1203994822
+DUAL BOOT - arvuti sisselogimisel otsustad kuhu operatsioonisüsteemi
+ …………………………………………………………………..
+INTERNET
+KIIRE WIFI: http://wut.ee/wifi Robootikaklubi wifi parol u4HNj3sgYK
+Koolivõrk on katkendlik, Mac on probleeme! Mis see on?? “HP võrgutüüp; access points on samad", aga proovi ka EDU ja ROBOOTIKA võrke
+…………………………………………………
+
+KEYBOARD @ ˇ~ˇ  
+*~Tilde asub tavaliselt üleval-vasakul on keyboard ~
+*FN all näitab miskeelne on klaviatuur
+WINDOWS downloadisime vajaliku, nagu:
+*NOTEPAD++ software
+Võta alla kogu zip package DESKTOP peale, ava, kasuta selle .exe
+*PuTTY krist@enos.itcollege.ee
+*Ctrl + L = teeb käsurea kirjetest puhtaks
+
+ASJAD LINUX
+/home/krist/
+/shared - siin on programme
+
+
+MAC MacBook PRO kasutamine
+*NB! MU KASUTAJA USER on: /Users/kylli/….
+***kopeerida kogu folderi  locationPATH, mine õun+i infoga näed seda
+*lähed faili ja vaatad, mis seal on kirjas sees: "Hello World!" sain ette: Terminalis käsuga (cd ~/Desktop/Java/Hello/) kausta sisse
+*Kuidagi annab terminali avamisel default kausta määrata, aga ma ei saanud seda esmapilgul tööle, jätsin katki.
+
+IDE teeb kogu selle tüütu töö ära:
+*kui algselt Tab Completion ei tööta, siis annab selle peale panna paari lihtsa käsuga: http://www.macdaddynews.com/2014/07/12/enable-tab-completion-mac-os-x-terminal/  
+*Vaikimisi on Mac'is kasutusel Bash shell. Kodukaustas on võimalik luua .bashrc, kuhu sisse saad kirjutada oma aliased mingitele käskudele, a'la:
+alias minu_programm='java /home/Kasutaja/Java/MinuProgramm'
+*see käsk uuendab ta operatsioonisüsteemi, nii nagu Windowsis on Update
+Neid aliaseid võid luua palju tahad. Mõistlik on neid luua tegevustele, mille kirjutamine võtab palju aega, kuid mida käivitad tihti. A'la Linuxis on mul tüüpiline alias "uptodate", mille sisuks on "sudo apt-get update && sudo apt-get upgrade && sudo apt-get dist-upgrade && sudo apt-get autoremove" - saan kõik need käsud käivitada ühe aliasega, ilma et peaks iga kord pikka lauset kirjutama
+*IntelliJ ja kiirklahvid Mac'il: https://resources.jetbrains.com/assets/products/intellij-idea/IntelliJIDEA_ReferenceCard_mac.pdf2
+
+
+UBUNTU
+*siin pole C, D kettad, süsteem algab kaldkriipsuga: / kirjutad täispika rea faili leidmiseks koos alamkataloogidega, vastasel juhul otsib ta faili sama kausta indexist
+*RESTART et liikuda Ubuntusse (ikoon hammasratas alt)
+*Ctrl+Alt+ T = TERMINAL Ubuntu
+*"Windows" hoiad all, kuvab shortcutte!!!
+*SINU asjad asuvad: Documents > Public_html 
+*MENÜÜ valik Ubuntu-IKOON tema enda märk viib kataloogidesse, järgnev valik *FAILID avab eraldi aknana, kus menüü alati vasakul ja uus tekib ka alumisele reale kuvatud kastis
+*MENÜÜ Ubuntu-ikoon = FIND, sellega kirjuta DISK: see näitab partitsioone
+*htop F10 TASK MANAGER
+*Ctrl + L = teeb käsurea kirjetest puhtaks
+
+TERMINAL-FTP ENOS SERVER (õnnetus)
+*Failid alumine-vasakul käsk: CONNECT TO SERVER, kirjuta sinna:
+sftp://krist@enos.itcollege.ee
+Seda saad teha ka mõne muu terminaliga või kodus oma arvutis: 
+ssh krist@enos.itcollege.ee  küsib parooli
+
+*FILE ZILLA software lae alla FTP win 64 (koolis oli olemas)
+
+*INTELLIJ
+https://www.jetbrains.com/idea/documentation/
+
+
+VÕTMED = käsud
+
+TERMINAL ssh krist@enos.itcollege.ee
+***kasuta TAB
+*ls    *dir
+*mv move
+*nano 
+*cat failinimi  
+*cp kopeeri
+*rm kustuta
+*cd ainult selle kasutamine viib sind alati otse algusesse home, avab-otsib katalooge
+*ls     =  cat  = nano   =  more (info jagatud lehtede kaupa)
+*ls public_html/
+*cat näitab mis on sees
+*cat quota-kasutus.txt näitab palju sinu ENOS serveris on sul ruumi
+*cat kui sisestad ilma järgneva sisendi andmisetta, jääb ootama konkreetset mida näidata
+*Ctrl+ C läheb EXIT käsust!!!
+*ctrl+L teeb käsuread puhtaks
+* -- help kaks kriipsu kirjutatakse sõna ette 
+*nano avab-otsib faile
+*nano "uus fail" kirjtas failinime ringi???
+*nano test.txt
+*ctrl +X = välja
+*ctrl +O = salvesta
+*ctrl +C = CANCEL välja 
+*TÜHIKUTE PUHUL FAILI NIMES pead kasutama tagurpidi kaldkriipsu  \\\
+cat uus/ fail 
+*gedit Documents/public_html/ avab selle graafiliselt
+*ifconfig -a kuvab asbul kõik koos peidetud süsteemiga
+*ls -a näitab ka peidetut, pikki faile, mis algavad punktiga 
+*ls -l näitab rohkem kaustu
+*ls -la näitab kaustu, faile, koos peidetuga
+*lsusb -D millised device kaasatud
+*lsusb --help näitab abi mis asi on
+*lsblh arvuti partitsioone
+*lsb_release -a ??? kas õigesti kirjutatud?  arvuti andmed
+*kernel lts = long time spport lts, kernel uuendamine võib juhtuda
+*Kasutaja õiguste rea lugemine kolmes osas: esimene näitab KASUTAJA, teine GRUPI, kolmas KÕIGI ÕIGUSEID (drwxr -xr -x+. 2 krist......)
+r lugeda aint tohib
+x jooksutada tohib
+w
+*dir
+*man mkdir avab manuaali, ning 
+suure hulga teksti sees saad kasutada otsingut Shift+ /    toksi otsitav!!!
+*rm -rf / ära tee, kustutab kõik f tähendab force, r tähendab rekursiivselt ehk alamkataloogilt alt kõik ka
+*man rm MANUAAL
+*arch mis protsessor on 32 bit 4 GIGA mälu
+*lcpc -h h on help
+*vi test.txt vi on miski hull keskkond hoopis teiste käskudega INSERT, noolenupud ei tööta ei reageeri. Välja saad ainult: ESC   :    q!
+*HOOKS ASJADE PRINT, mis on folderis: Kullis-MacBook-Pro:HOOKS kylli$ ls *  > export.txt
+*kasutades cd lähed alati algalgusesse, sattusin home ja ei osanud sealt oma arvutisse tagasi ronida. See aitas:
+Kullis-MacBook-Pro:home kylli$ cd /Users/kylli/Desktop
+*mkdir .ssh  - lood kataloogi  mkdir code
+*rmdir .ssh/authorized_keys KUSTUTAB
+*clear või ctrl L teeb terminali akna puhtaks
+*kopeeri cp  ~/Desktop/POMM/pomm.java .
+
+minu MAC taga Git BASH sama asi sisselogimine
+ssh-keygen -t ecdsa -P “"
+…………….
+Zoc asemel PuTTY = Putty GEN
+kuna seal peab alati parooli trükkima, siis on olemas salvestamiseks Putty GEN ehk putty key, mis 2048 bit RSS (mis on mobiiliIDs ka muideks)
+Kuidas see toimib, ta teeb avaliku võtme ja teeb teise privaatse võtme authorized keys
+
+versioonihaldus GIT samuti teeb sama….
+
+remote host (sihtaadress) - ssh
+local host - ssh klient
+…………………………………………………………………..
+VIRTUALBOX  - ei soovitata; kasutades WIN10 nõuab mahtu ketast 50 GB ja Ubuntu mate 15 (Ubuntu mate on teisest Ubuntust parem, opereerib paremini). 32 bit versioon võib osutada aeglaseks. Virtualiseemine tugi. BIOS-sees. Näiteks, MAC tihti on virtualiseerimine väljalülitatud. WIFI nett: seadistasime sama, et oleks minu põhiarvutiga ühenduses = BRIDGE nett. Kuidas ta seda tegI??? 2 VARIANTI: DOUALBOOT laadides arvutisse või VirtualBox
+
+
+
+
+..................................
+
+AINE = IT EELKURSUS
+avalik või privaatsed võtmed sisselogimiseks
+Milleks see? =saab arvutist sisse logida enosesse või githubi ilma et küsiks parooli
+
+kus need mu MacBook arvutis asuvad: Last login: Sun Oct  9 17:54:34 on ttys001
+Kullis-MacBook-Pro:.ssh kylli$ ls
+github_rsa github_rsa.pub id_ecdsa id_ecdsa.pub id_rsa id_rsa.pub known_hosts
+Kullis-MacBook-Pro:.ssh kylli$ cat id_rsa.pub
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDPCuafUXCCm2BRzZCU4Cka+T3P1st7CtW9s2gmqRoanKRJHFhc2/WllOx9uJ0R9VreInjMzEw9dQrlWDQ5QtCc980kM8NvULZo/qHopO1G3NoFfYegfB1753y5c8fyEaHYum670PmxKCnDfd7O3kG8ts9mSGFycihIdad+sUYm5shP0EQOS/TZu82xU7PPMoroThDXTFf1QGqnaUGYNgJzKo9IoORSL/o5X8NXnS0WFLCoPUMxiSCbF0v6FFrzU66+i2ppTA/lNX7iPGHnKxutMhgnJBMUIlOTBDqwOQqk72rizdqRbqyioBXDoFTYhagGoiO0Wk7s4+h1HkC05eD7 kylli@Kullis-MacBook-Pro.local
+……………………………………………...
+
+a) sshfs on MAC et TO DOWNLOAD This is a filesystem client based on the SSH File Transfer Protocol. GitHub - libfuse/sshfs: A network filesystem client to connect to SSH 
+
+PRIVATE KEY CREATING ON local machine mac:
+Kullis-MacBook-Pro:Desktop kylli$ ssh-copy-id
+-bash: ssh-copy-id: command not found
+Kullis-MacBook-Pro:Desktop kylli$ cat .ssh/
+Kullis-MacBook-Pro:Desktop kylli$ cd
+Kullis-MacBook-Pro:~ kylli$ cat .ssh/
+github_rsa      github_rsa.pub  id_ecdsa        id_ecdsa.pub    known_hosts     
+Kullis-MacBook-Pro:~ kylli$ cat .ssh/id_ecdsa
+id_ecdsa      id_ecdsa.pub  
+Kullis-MacBook-Pro:~ kylli$ cat .ssh/id_ecdsa.pub 
+ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBEPw406sLIU+cR04tO/Nj9nEkDRgqYuYVj+MpJ/y0uTBfo2wvKHeaEziwi1/hIvrDugB1jXvOpuehqNjBY0I9DI= kylli@Kullis-MacBook-Pro.local
+b) siis tuleb see kopeerida oma enos serveris, teed seal lahti faili nimega authorized_keys ja kopeerid õlevaloleva lingi sinna. Ctrl X hakkab faili kinni pannes küsima, kas salvestada
+nano 2.2.6                    File: .ssh/authorized_keys
+ 
+krist@enos.itcollege.ee
+ssh-keygen   =>    loob sellise folderi, milles tekib kaks võtit:   ~/.ssh 
+id_rsa on salajane võtmepaari pool
+id_rsa.pub on avalik võtmepaari pool
+ 
+SAMMUD
+ssh-keygen			(teeb: generating public+private rsa key par (/home/krist/.ssh/id.rsa)        It created directory ‘/home/krist/.ssh'
+enter 			ei pea midagi panema
+enter 			ei pea midagi panema
+cd .ssh
+ls
+cat id_rsa.pub		see näitab ära genereeritud võtme avalikus võtmepaari pooles!!
+siis kopeeidki õigesse sihtarvutisse: ssh-copy-id krist@enos.itcollege.ee
+TULEMUS: proovi ssh minna sisse enosesse, kui ta ei küsi paroole ja läheb sisse, siis ju toimibki
+…..
+LINUZ ja MAC ssh on sisseehitatud. Windowsis peab panema sisse aga putty ja puttygen ning selle alla laadima
+
+
+
+
+
+
+
+
+
 
 
 
