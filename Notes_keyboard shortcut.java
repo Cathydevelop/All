34c1175d1a0063018a8f94d05c0e84c30e3a7ead
refocus <refocustest@gmail.com>
2017-01-27 22:21:16 +0200

    keyboard shortcuts links

---
+++ Notes_keyboard shortcut.java
@@ -0,0 +1,33 @@
+KEYBOARD shortcuts + ING SÕNAD
+SHIFT (nool üles)+  fn, ctrl, ALT, cmd
+…
+⇧ represents SHIFT
+ctrl:	 ^ represents CONTROL
+alt: 	⌥ symbol on it OPTION
+cmd: ⌘ representing COMMAND 
+http://www.howtogeek.com/183636/how-the-command-and-option-keys-work-on-a-mac/
+
+⌘ ; :   ~ §   if else && || 
+{ }  looksulud	option + 7 
+[ ]			option + 8
+!	shift + 1 on hüümärk!
+\  	Alt+plus sign (tagurpidi kaldkriips)
+€
+@	Alt+2 commercial at
+|	vertical bar (the pipe) püstkriips 	Alt+<
+‘ single quotes '
+“ double quotes “
+# 	trellid HASH(tag) (pound sign but UK pound sign is £)(musical sign it’s a sharp)
+// 	/* kaldkriips tärn //kommentaar SLASH /* */
+Dash mõttekriips
+Slash kaldkriips an oblique stroke (/) used
+$	option + 4 = $
+ˇ (vasakul, ülevalt teine nupp)
+¶ (vasakul, ülevalt teine nupp + Alt)
+</B> 
+> niipidi nool shift hoida all
+
+Shift+alt+K =plain text copy on MacBook
+
+reavahe
+\n
