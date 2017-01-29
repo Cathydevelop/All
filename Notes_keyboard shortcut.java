e6950762e9dcfbcf238b557f1665b790fcef6c13
refocus <refocustest@gmail.com>
2017-01-29 10:04:44 +0200

    Update Notes_keyboard shortcut.java

--- Notes_keyboard shortcut.java
+++ Notes_keyboard shortcut.java
@@ -44,3 +44,44 @@ Shift+alt+K =plain text copy on MacBook
 
 reavahe
 \n
+
+
+
+krist@klass1-17-l:~$ cd Documents
+krist@klass1-17-l:~/Documents$ ls
+Desktop               kool IGASUGU EXCEL VBA to solve  quota-kasutus.txt
+DropboxInstaller.exe  Music                            Templates
+Favorites             Pictures                         Videos
+KOOD                  public_html
+krist@klass1-17-l:~/Documents$ git clone git@eksam.koodikool.ee:tudeng/KNgJtw29zvLMSDjmp.git
+Cloning into 'KNgJtw29zvLMSDjmp'...
+The authenticity of host 'eksam.koodikool.ee (128.199.38.175)' can't be established.
+ECDSA key fingerprint is SHA256:ccCaJkGKU2napJQ806Jol+9VZa+pjDf3zTgHsXE69Ck.
+Are you sure you want to continue connecting (yes/no)? yes
+Warning: Permanently added 'eksam.koodikool.ee,128.199.38.175' (ECDSA) to the list of known hosts.
+git@eksam.koodikool.ee's password: 
+remote: Counting objects: 5, done.
+remote: Compressing objects: 100% (3/3), done.
+remote: Total 5 (delta 0), reused 0 (delta 0)
+Receiving objects: 100% (5/5), done.
+Checking connectivity... done.
+krist@klass1-17-l:~/Documents$ git status
+fatal: Not a git repository (or any parent up to mount point /home/krist/Documents)
+Stopping at filesystem boundary (GIT_DISCOVERY_ACROSS_FILESYSTEM not set).
+krist@klass1-17-l:~/Documents$ ^C
+krist@klass1-17-l:~/Documents$ ^C
+krist@klass1-17-l:~/Documents$ git init
+Initialized empty Git repository in /home/krist/Documents/.git/
+krist@klass1-17-l:~/Documents$ cd 
+krist@klass1-17-l:~$ cd 
+krist@klass1-17-l:~$ cd Documents
+krist@klass1-17-l:~/Documents$ mkdir UUS
+krist@klass1-17-l:~/Documents$ cd UUS
+krist@klass1-17-l:~/Documents/UUS$ git init
+Initialized empty Git repository in /home/krist/Documents/UUS/.git/
+krist@klass1-17-l:~/Documents/UUS$ git clone git@eksam.koodikool.ee:tudeng/KNgJtw29zvLMSDjmp.git
+Cloning into 'KNgJtw29zvLMSDjmp'...
+git@eksam.koodikool.ee's password: 
+Permission denied, please try again.
+git@eksam.koodikool.ee's password: 
+
