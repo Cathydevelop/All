3efa21b2ccef0cb608969cc878f345c941e9c39a
refocus <refocustest@gmail.com>
2017-02-01 02:21:01 +0200

    Update YT.java

--- YT.java
+++ YT.java
@@ -1,1 +1,4 @@
-YT
+YT once to learn, IntelliJ tricks: https://www.youtube.com/watch?v=eq3KiAH4IBI
+
+5-1 Objektorienteeritus
+https://www.youtube.com/watch?v=cGUMn0gzWvg&list=PLWkgPad9kpY3IWcuywjrCVQgFiF5MhEMp&index=14
