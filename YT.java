52303f64b283acfea3e172989b33974638d4b36b
refocus <refocustest@gmail.com>
2017-02-02 09:55:01 +0200

    Update YT.java

--- YT.java
+++ YT.java
@@ -1,4 +1,30 @@
 YT once to learn, IntelliJ tricks: https://www.youtube.com/watch?v=eq3KiAH4IBI
 
-5-1 Objektorienteeritus
-https://www.youtube.com/watch?v=cGUMn0gzWvg&list=PLWkgPad9kpY3IWcuywjrCVQgFiF5MhEMp&index=14
+
+i200 1-1 Sissejuhatus: i200, kuidas täiendada; õppetöö online vorm, eesmärk 3 kuuga nooremprogrameerijaks; oma projekt ja iganädalased ülessanded, õppematerjal ja kommuunikool koodiõppimiseks foorumis
+i200 1-2 Programmeerimisest üldisemalt
+i200 1-3 Git ja Laevade Pommitamise algus: Git vajalikud bash käsud koodi pushimiseks näiteks GitHub kontosse
+i200 2-1 Loogilised operaatorid ja booleanid: 
+BOOLEAN true false IF jaoks, !a NB!HÜÜMÄRK SEE ON BOOLEANITE OMADUS, && (and) || (or)
+https://www.youtube.com/watch?v=gjxBTo37n8M&index=4&list=PLWkgPad9kpY3IWcuywjrCVQgFiF5MhEMp
+ //ternary operator operation in java  //https://www.tutorialspoint.com/java/java_basic_operators.htm
+
+
+2 IDE firstrun 
+https://www.youtube.com/watch?v=bs5U5kCb1I4&list=PLWkgPad9kpY1Ns3Q23g1qAlE9vQbmpSKn&index=5
+intelliJ seadistamine, pakett, klass loomine
+
+2 2 IDE debug 
+https://www.youtube.com/watch?v=2Ju3yuBtop4&index=4&list=PLWkgPad9kpY1Ns3Q23g1qAlE9vQbmpSKn
+debuggimine
+
+5-1 Objektorienteeritus: 
+https://www.youtube.com/watch?v=cGUMn0gzWvg&index=14&list=PLWkgPad9kpY3IWcuywjrCVQgFiF5MhEMp
+klasside loomine, for-tsükkel, mida meetod võtab vastu ja mida tagastab ehk programmi loogika; 
+näited, kuidas kutsud (punktiga) välja teise klassi meetodeid ja väärtusi;
+public vs private, protected
+
+5-3 Klassi importimine
+miks me ei pea siis kassat importima, sest ta on main fail kõrval, siis ei pea
+5-5 Projekti struktuur
+MVC Model View Controller
